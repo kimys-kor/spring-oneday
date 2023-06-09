13276d6a11ed44de3de857d8b2cd8a716a68d30c
kimys-kor <ys2life1@naver.com>
2023-06-10 00:50:51 +0900

    멤버 -> 유저

--- api/src/main/java/com/oneday/api/controller/UserController.java
+++ api/src/main/java/com/oneday/api/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/member")
 @RequiredArgsConstructor
-public class MemberController {
+public class UserController {
 
     private final UserService userService;
 
